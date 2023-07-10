76d7eb63922ad882ccf002fa7f85394a4718150c
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2023-07-10 23:24:12 +0300

    Add buttons for swiper

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,12 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
+
     spaceBetween: 31,
+
     loop: true,
+
+    navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
